1d036d3fb9a846b08f25f07cf1411ffac6a56438
wutperson <68948211+wutperson@users.noreply.github.com>
2020-08-17 14:59:15 +0300

    Add files via upload

---
+++ Assets/Scripts/GameFunctionality.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameFunctionality : MonoBehaviour
+{
+    public AudioClip clickSound;
+    private AudioSource source { get { return GetComponent<AudioSource>(); } }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.AddComponent<AudioSource>();
+        source.clip = clickSound;
+        source.playOnAwake = false;
+    }
+
+
+    public void OnGiveUpButtonClick()
+    {
+        source.PlayOneShot(clickSound);
+    }
+}
